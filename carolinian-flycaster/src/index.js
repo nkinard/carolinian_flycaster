888a683b28e4d70ac8a907e2619f1eb2e28f557d
nkinard <nkinard7503@gmail.com>
2025-03-25 09:46:13 -0400

    react

--- carolinian-flycaster/src/index.js
+++ carolinian-flycaster/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
+import About from './pages/About';
+import Layout from './Layout';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
