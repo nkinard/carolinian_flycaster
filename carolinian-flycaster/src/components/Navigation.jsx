5282e5a0b1a74c241010036bbfd518d768787ee3
nkinard <nkinard7503@gmail.com>
2025-04-01 09:44:06 -0400

    update nav in class

--- carolinian-flycaster/src/components/Navigation.jsx
+++ carolinian-flycaster/src/components/Navigation.jsx
@@ -1,10 +1,24 @@
 import './css/Navigation.css';
-import {Outlet, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
+import { useState } from "react";
 
 function Navigation() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav id="main-nav">
-            <ul>
+            <section>
+                <div id="hamburger-toggle" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </section>
+            <ul className={menuOpen?" ":"hide-small"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/Guide">Fly Fishing Guide</Link></li>
                 <li><Link to="/Library">Fish and Fly Library</Link></li>
